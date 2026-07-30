487859a875a4c6c69207acf4c6f0fcadf48d2d1d
Giorgia <giorgiarnld@proton.me>
2026-07-30 19:10:07 +0300

    Create DashboardConfig.kt

---
+++ src/main/kotlin/dashboard/config/DashboardConfig.kt
@@ -0,0 +1,17 @@
+package dashboard.config
+
+object DashboardConfig {
+
+    const val HOME_NAME =
+        "Smart Home"
+
+    const val LOW_BATTERY_LEVEL =
+        20
+
+    const val HIGH_TEMPERATURE =
+        30.0
+
+    const val REFRESH_INTERVAL =
+        5
+
+}
